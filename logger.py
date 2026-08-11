8e1afd0a1260943cf22ff99391c78d335321048c
Mara Hoffmann <larasmahesa3@gmail.com>
2026-08-11 17:15:29 +0200

    Add input validation in main processing loop

---
+++ logger.py
@@ -0,0 +1,29 @@
+import logging
+
+class Logger:
+    def __init__(self, name='cli-helper-56'):
+        self.logger = logging.getLogger(name)
+        self.logger.setLevel(logging.DEBUG)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+        handler.setFormatter(formatter)
+        self.logger.addHandler(handler)
+
+    def debug(self, msg):
+        self.logger.debug(msg)
+
+    def info(self, msg):
+        self.logger.info(msg)
+
+    def warning(self, msg):
+        self.logger.warning(msg)
+
+    def error(self, msg):
+        self.logger.error(msg)
+
+    def critical(self, msg):
+        self.logger.critical(msg)
+
+if __name__ == '__main__':
+    logger = Logger()
+    logger.info('Logger initialized.')
