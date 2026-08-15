580fe284f4fc3dfd201ec6153691a5a70cb285a1
Mara Hoffmann <larasmahesa3@gmail.com>
2026-08-15 17:14:12 +0200

    Update logger setup with rotation

--- logger.py
+++ logger.py
@@ -1,29 +1,16 @@
 import logging
+from logging.handlers import RotatingFileHandler
 
-class Logger:
-    def __init__(self, name='cli-helper-56'):
-        self.logger = logging.getLogger(name)
-        self.logger.setLevel(logging.DEBUG)
-        handler = logging.StreamHandler()
-        formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
-        handler.setFormatter(formatter)
-        self.logger.addHandler(handler)
+def setup_logger(log_file='game.log', max_bytes=5 * 1024 * 1024, backup_count=3):
+    logger = logging.getLogger('game_logger')
+    logger.setLevel(logging.DEBUG)
+    handler = RotatingFileHandler(log_file, maxBytes=max_bytes, backupCount=backup_count)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    handler.setFormatter(formatter)
+    logger.addHandler(handler)
+    return logger
 
-    def debug(self, msg):
-        self.logger.debug(msg)
-
-    def info(self, msg):
-        self.logger.info(msg)
-
-    def warning(self, msg):
-        self.logger.warning(msg)
-
-    def error(self, msg):
-        self.logger.error(msg)
-
-    def critical(self, msg):
-        self.logger.critical(msg)
-
+# Example usage:
 if __name__ == '__main__':
-    logger = Logger()
-    logger.info('Logger initialized.')
+    logger = setup_logger()
+    logger.info('Logging has started!')
